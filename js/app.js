32ed89ff6b09c9574149d2027f3babfb99f77aa8
Aryslan Duisebay <arsilan.duisebay@gmail.com>
2023-05-19 12:59:03 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -1,96 +1,13 @@
-function myFunc1() {
-    const second  = document.querySelector('.Working-Process__box-bottom');
-    const element = document.querySelector('.Working-Process__box');
-    const image = document.querySelector(".image-active");
-    if(second.style.display == 'none') {
-        element.style.backgroundColor = '#B9FF66';
-        second.style.display = 'flex';
-        image.src="images/Plus-icon-active.png"
-        
-    } else {
-        element.style.backgroundColor = '#F3F3F3';
-        second.style.display = 'none';
-        image.src="images/Plus-icon-non-active.png"
-    }
-}
-
-function myFunc2() {
-    const second  = document.querySelector('.bottom2');
-    const element = document.querySelector('.box2');
-    const image = document.querySelector(".a2");
-    if(second.style.display == 'none') {
-        element.style.backgroundColor = '#B9FF66';
-        second.style.display = 'flex';
-        image.src="images/Plus-icon-active.png"
-        
-    } else {
-        element.style.backgroundColor = '#F3F3F3';
-        second.style.display = 'none';
-        image.src="images/Plus-icon-non-active.png"
-    }
-}
-
-function myFunc3() {
-    const second  = document.querySelector('.bottom3');
-    const element = document.querySelector('.box3');
-    const image = document.querySelector(".a3");
-    if(second.style.display == 'none') {
-        element.style.backgroundColor = '#B9FF66';
-        second.style.display = 'flex';
-        image.src="images/Plus-icon-active.png"
-        
-    } else {
-        element.style.backgroundColor = '#F3F3F3';
-        second.style.display = 'none';
-        image.src="images/Plus-icon-non-active.png"
-    }
-}
-
-function myFunc4() {
-    const second  = document.querySelector('.bottom4');
-    const element = document.querySelector('.box4');
-    const image = document.querySelector(".a4");
-    if(second.style.display == 'none') {
-        element.style.backgroundColor = '#B9FF66';
-        second.style.display = 'flex';
-        image.src="images/Plus-icon-active.png"
-        
-    } else {
-        element.style.backgroundColor = '#F3F3F3';
-        second.style.display = 'none';
-        image.src="images/Plus-icon-non-active.png"
-    }
-}
-
-function myFunc5() {
-    const second  = document.querySelector('.bottom5');
-    const element = document.querySelector('.box5');
-    const image = document.querySelector(".a5");
-    if(second.style.display == 'none') {
-        element.style.backgroundColor = '#B9FF66';
-        second.style.display = 'flex';
-        image.src="images/Plus-icon-active.png"
-        
-    } else {
-        element.style.backgroundColor = '#F3F3F3';
-        second.style.display = 'none';
-        image.src="images/Plus-icon-non-active.png"
-    }
-}
-
-function myFunc6() {
-    const second  = document.querySelector('.bottom6');
-    const element = document.querySelector('.box6');
-    const image = document.querySelector(".a6");
-    if(second.style.display == 'none') {
-        element.style.backgroundColor = '#B9FF66';
-        second.style.display = 'flex';
-        image.src="images/Plus-icon-active.png"
-        
-    } else {
-        element.style.backgroundColor = '#F3F3F3';
-        second.style.display = 'none';
-        image.src="images/Plus-icon-non-active.png"
-    }
+let coll = document.getElementsByClassName('Working-Process__box-button');
+for(let i=0; i<coll.length; i++) {
+    coll[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if(content.style.display == 'none') {
+            content.style.display = 'flex';
+        } else {
+            content.style.display = 'none';
+        }
+    })
 }
 
